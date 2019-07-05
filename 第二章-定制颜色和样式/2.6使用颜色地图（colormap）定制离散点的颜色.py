e3dbae4bfaf5afbfc1e41b4950501bd98dd0ae9b
guoqiang <gqworker@163.com>
2019-07-05 17:05:29 +0800

    '2.6使用颜色地图（colormap）定制离散点的颜色'

---
+++ 第二章-定制颜色和样式/2.6使用颜色地图（colormap）定制离散点的颜色.py
@@ -0,0 +1,19 @@
+# 2.6使用颜色地图（colormap）定制离散点的颜色
+import numpy
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+
+N = 256
+angle = numpy.linspace(0,8*2*numpy.pi,N)
+radius = numpy.linspace(0.5,1,N)
+X = radius * numpy.cos(angle)
+Y = radius * numpy.sin(angle)
+# plt.scatter(X,Y,c = angle,cmap=cm.hsv)
+plt.scatter(X,Y,c = angle,cmap=cm.hot)
+plt.show()
+
+
+
+
+
