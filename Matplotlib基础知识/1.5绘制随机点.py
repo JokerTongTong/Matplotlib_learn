707499f49395a9a256ba70af45ca51e309697c17
guoqiang <gqworker@163.com>
2019-07-05 11:23:25 +0800

    '1.5绘制随机点'

--- Matplotlib基础知识/1.5绘制随机点.py
+++ Matplotlib基础知识/1.5绘制随机点.py
@@ -1,1 +1,15 @@
 # 1.5绘制随机点
+
+# plot 将所有的点连成一条曲线
+# scatter 绘制随机点
+
+import random
+import matplotlib.pyplot as plt
+
+count = 1024
+X = [random.random() * 100 for i in range(count)]
+Y = [random.random() * 100 for i in range(count)]
+print(X)
+print(Y)
+plt.scatter(X,Y)
+plt.show()
