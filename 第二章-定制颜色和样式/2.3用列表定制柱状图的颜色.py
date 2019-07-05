d680a67ae29a3e0e950598bc0df28e783ab82a56
guoqiang <gqworker@163.com>
2019-07-05 16:43:36 +0800

    '2.3用列表定制柱状图的颜色'

---
+++ 第二章-定制颜色和样式/2.3用列表定制柱状图的颜色.py
@@ -0,0 +1,13 @@
+# 2.3用列表定制柱状图的颜色
+import numpy
+import random
+import matplotlib.pyplot as plt
+
+values = [random.randint(0,99) for i in range(50)]
+color_set = ['#FF00FF','#FFFF00','r','b']
+color_list = [color_set[random.randint(0,3)] for i in range(100)]
+plt.bar(range(len(values)),values,color=color_list)
+plt.show()
+
+
+
