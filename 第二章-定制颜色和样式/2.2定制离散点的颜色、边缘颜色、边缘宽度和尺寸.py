28f8ba8e259f5c2cb471abcc73d71d2a549b2844
guoqiang <gqworker@163.com>
2019-07-05 16:35:45 +0800

    '2.2定制离散点的颜色、边缘颜色、边缘宽度和尺寸'

---
+++ 第二章-定制颜色和样式/2.2定制离散点的颜色、边缘颜色、边缘宽度和尺寸.py
@@ -0,0 +1,26 @@
+# 2.2定制离散点的颜色、边缘颜色、边缘宽度和尺寸
+import numpy
+import matplotlib.pyplot as plt
+
+
+A = numpy.random.standard_normal((100,2))
+print(A)
+# print(A[:,0])
+A += numpy.array((-1,-1))
+print(A)
+
+B = numpy.random.standard_normal((100,2))
+B += numpy.array((1,1))
+colors = ['r','yellow','b','c','#FF00FF','0.75']
+plt.scatter(A[:,0],A[:,1],color=colors[0])
+plt.scatter(B[:,0],B[:,1],color=colors[2])
+plt.scatter(A[:,0],B[:,1],color=colors[4],edgecolor=colors[2],s=200,linewidth=3)
+# edgecolor:边缘色,
+# s:像素，大小
+# linewidth:边缘的线宽度
+
+plt.show()
+
+
+
+
