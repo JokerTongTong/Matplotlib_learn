0460c0e9580dd46382b86d541662ddd91f52acdf
guoqiang <gqworker@163.com>
2019-07-05 11:53:27 +0800

    .1.7回值多组垂直和水平柱状图'

---
+++ Matplotlib基础知识/1.7绘制多组垂直和水平柱状图.py
@@ -0,0 +1,20 @@
+# 1.7绘制多组垂直和水平柱状图
+import matplotlib.pyplot as plt
+
+data = [[5,25,50,20],
+        [4,34,65,13],
+        [6,22,53,19]]
+
+'''
+# 垂直
+plt.bar(range(4),data[0],width=0.25)
+plt.bar([x + 0.25 for x in range(4)],data[1],width=0.25)
+plt.bar([x + 0.5 for x in range(4)],data[2],width=0.25)
+plt.show()
+'''
+
+# 水平
+plt.barh(range(4),data[0],height=0.25)
+plt.barh([x + 0.25 for x in range(4)],data[1],height=0.25)
+plt.barh([x + 0.5 for x in range(4)],data[2],height=0.25)
+plt.show()
