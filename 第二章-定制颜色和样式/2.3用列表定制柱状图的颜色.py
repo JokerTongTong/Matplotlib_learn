38f5c11e7385d213bd23c6f2a5457e89b35d4c73
guoqiang <gqworker@163.com>
2019-07-08 17:19:40 +0800

    'random.randint()随机整数，区间左闭右闭'

--- 第二章-定制颜色和样式/2.3用列表定制柱状图的颜色.py
+++ 第二章-定制颜色和样式/2.3用列表定制柱状图的颜色.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 values = [random.randint(0,99) for i in range(50)]
 color_set = ['#FF00FF','#FFFF00','r','b']
-color_list = [color_set[random.randint(0,3)] for i in range(100)]
+color_list = [color_set[random.randint(0,3)] for i in range(100)]  # random.randint()随机整数，区间左闭右闭
 plt.bar(range(len(values)),values,color=color_list)
 plt.show()
 
