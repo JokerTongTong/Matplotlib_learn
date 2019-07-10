623d7db7bfd44064c5c4436eede81dcd56c3acfc
guoqiang <gqworker@163.com>
2019-07-10 17:08:24 +0800

    '颜色地图进一步完善笔记'

--- 第二章-定制颜色和样式/2.6使用颜色地图（colormap）定制离散点的颜色.py
+++ 第二章-定制颜色和样式/2.6使用颜色地图（colormap）定制离散点的颜色.py
@@ -1,11 +1,13 @@
 # 2.6使用颜色地图（colormap）定制离散点的颜色
 import numpy
-import matplotlib.cm as cm
+import matplotlib.cm as cm  # 颜色地图不同的地图在里面定义
 import matplotlib.pyplot as plt
 
 
 N = 256
+# 旋转的角度  2pi 一圈，16pi 八圈
 angle = numpy.linspace(0,8*2*numpy.pi,N)
+# 半径
 radius = numpy.linspace(0.5,1,N)
 X = radius * numpy.cos(angle)
 Y = radius * numpy.sin(angle)
